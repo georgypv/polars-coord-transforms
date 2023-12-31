11e6eb66590aed22203ec1fec209a4533049b982
GeorgyPopov <originalno93@yandex.ru>
2023-12-31 15:49:22 +0300

    add rust tests

--- src/coord_transforms.rs
+++ src/coord_transforms.rs
@@ -17,3 +17,30 @@ pub fn ecef_to_lla_elementwise(x: f64, y: f64, z: f64) -> (f64, f64, f64) {
     (rad2deg(lla.1), rad2deg(lla.0), lla.2)
 }
 
+
+#[cfg(test)]
+mod transform_tests { 
+    use crate::coord_transforms::{enu_to_ecef_elementwise, ecef_to_lla_elementwise};
+
+    #[test]
+    fn test_enu_to_ecef() {
+        let enu_coords: Vec<f64> = vec![-97066.730132, 122807.787398,	-1888.737721];
+        let rotation: Vec<f64> = vec![0.13007119, 0.26472049, 0.85758219, 0.42137553];
+        let offset: Vec<f64> = vec![2852423.40536658, 2201848.41975346, 5245234.74365368];
+
+        let expected_result: (f64, f64, f64) = (2830593.6327610738, 2062375.5703225536, 5312896.0721501345);
+
+        assert_eq!(enu_to_ecef_elementwise(enu_coords, rotation, offset), expected_result)
+
+    }
+
+    #[test]
+    fn test_ecef_to_lla() {
+        
+        let ecef_coords: (f64, f64, f64) = (2830593.6327610738, 2062375.5703225536, 5312896.0721501345);
+        let expected_result: (f64, f64, f64) = (36.077147686805766, 56.783927007002866, 165.8986865637805);
+
+        assert_eq!(ecef_to_lla_elementwise(ecef_coords.0, ecef_coords.1, ecef_coords.2), expected_result)
+    }
+
+}
--- src/s2_functions.rs
+++ src/s2_functions.rs
@@ -15,3 +15,38 @@ pub fn cellid_to_lonlat_elementwise(cellid: u64) -> (f64, f64) {
     let lng = lnglat.lng.deg();
     (lng, lat)
 }
+
+
+
+#[cfg(test)]
+mod s2_tests {
+
+    use crate::s2_functions::{lonlat_to_cellid_elementwise, cellid_to_lonlat_elementwise};
+
+    #[test]
+    fn test_lonlat_to_cellid() {
+        let lon: f64 = 36.077147686805766;
+        let lat: f64 = 56.783927007002866;
+        let level: u64 = 30;
+        let expected_cellid: u64 = 5095400969591719543;
+
+        assert_eq!(lonlat_to_cellid_elementwise(lon, lat, level), expected_cellid);
+    }
+
+    #[test]
+    fn test_cellid_to_lonlat() {
+        let cellid: u64 = 5095400969591719540;
+        let expected_lonlat: (f64, f64) = (36.077147799420544, 56.78392700893653);
+        
+        assert_eq!(cellid_to_lonlat_elementwise(cellid), expected_lonlat)
+    }
+
+    #[test]
+    fn s2_circular_transformation() {
+        let cellid: u64 = 5095400969591719543; //level 30 S2 cell
+        let (lon, lat) = cellid_to_lonlat_elementwise(cellid);
+
+        assert_eq!(lonlat_to_cellid_elementwise(lon, lat, 30), cellid)
+    }
+    
+}
