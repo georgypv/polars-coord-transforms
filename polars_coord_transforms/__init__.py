be05478bb7a1c7083b8e17d7ef0efe3fcea8f934
GeorgyPopov <originalno93@yandex.ru>
2024-01-02 21:25:06 +0300

    ruff format

--- polars_coord_transforms/__init__.py
+++ polars_coord_transforms/__init__.py
@@ -7,46 +7,40 @@ from typing import Protocol, Iterable, cast
 
 lib = _get_shared_lib_location(__file__)
 
+
 @pl.api.register_expr_namespace("s2")
 class S2NameSpace:
-
     def __init__(self, expr: pl.Expr):
         self._expr = expr
 
     def lonlat_to_cellid(self, level: int = 30) -> pl.Expr:
-
         if level < 1 or level > 30:
             raise ValueError("`level` parameter must be between 1 and 30!")
-        
+
         return self._expr.register_plugin(
             lib=lib,
             symbol="lonlat_to_cellid",
             is_elementwise=True,
-            kwargs={"level": level}
+            kwargs={"level": level},
         )
-    
+
     def cellid_to_lonlat(self) -> pl.Expr:
         return self._expr.register_plugin(
-            lib=lib,
-            symbol="cellid_to_lonlat",
-            is_elementwise=True
+            lib=lib, symbol="cellid_to_lonlat", is_elementwise=True
         )
 
     def cell_contains_point(self, point: pl.Expr) -> pl.Expr:
         return self._expr.register_plugin(
-            lib=lib,
-            symbol="cell_contains_point",
-            is_elementwise=True,
-            args=[point]
-        ) 
-    
+            lib=lib, symbol="cell_contains_point", is_elementwise=True, args=[point]
+        )
+
     def cellid_to_vertices(self) -> pl.Expr:
         return self._expr.register_plugin(
             lib=lib,
             symbol="cellid_to_vertices",
             is_elementwise=True,
         )
-    
+
 
 @pl.api.register_expr_namespace("transform")
 class TransformNameSpace:
@@ -55,35 +49,28 @@ class TransformNameSpace:
 
     def enu_to_ecef(self, rotation: pl.Expr, offset: pl.Expr) -> pl.Expr:
         return self._expr.register_plugin(
-            lib=lib,
-            symbol="enu_to_ecef",
-            is_elementwise=True,
-            args=[rotation, offset]
+            lib=lib, symbol="enu_to_ecef", is_elementwise=True, args=[rotation, offset]
         )
-    
+
     def ecef_to_enu(self, rotation: pl.Expr, offset: pl.Expr) -> pl.Expr:
         return self._expr.register_plugin(
-            lib=lib,
-            symbol="ecef_to_enu",
-            is_elementwise=True,
-            args=[rotation, offset]
+            lib=lib, symbol="ecef_to_enu", is_elementwise=True, args=[rotation, offset]
         )
-    
-    
+
     def ecef_to_lla(self) -> pl.Expr:
         return self._expr.register_plugin(
             lib=lib,
             symbol="ecef_to_lla",
             is_elementwise=True,
         )
-    
+
     def lla_to_ecef(self) -> pl.Expr:
         return self._expr.register_plugin(
             lib=lib,
             symbol="lla_to_ecef",
             is_elementwise=True,
         )
-    
+
 
 class CoordTransformExpr(pl.Expr):
     @property
@@ -93,23 +80,23 @@ class CoordTransformExpr(pl.Expr):
     @property
     def transform(self) -> TransformNameSpace:
         return TransformNameSpace(self)
-    
+
 
 class CTColumn(Protocol):
     def __cal__(
-            self,
-            name:  str | PolarsDataType | Iterable[str] | Iterable[PolarsDataType],
-            *more_names: str | PolarsDataType,
+        self,
+        name: str | PolarsDataType | Iterable[str] | Iterable[PolarsDataType],
+        *more_names: str | PolarsDataType,
     ) -> CoordTransformExpr:
         ...
-    
+
     def __getattr__(self, name: str) -> pl.Expr:
         ...
-    
+
     @property
     def s2(self) -> S2NameSpace:
         ...
-    
+
     @property
     def transform(self) -> TransformNameSpace:
         ...
@@ -117,6 +104,4 @@ class CTColumn(Protocol):
 
 col = cast(CTColumn, pl.col)
 
-__all__ = [
-    "col"
-]
+__all__ = ["col"]
