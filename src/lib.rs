b34c5f8e17fba6bbe4b75b3bc6e2014d24f8d213
GeorgyPopov <originalno93@yandex.ru>
2024-01-13 19:54:15 +0300

    add Jemalloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
 mod s2_functions;
 mod coord_transforms;
 mod expressions;
+
+#[cfg(target_os = "linux")]
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+#[cfg(target_os = "linux")]
+static ALLOC: Jemalloc = Jemalloc; 
